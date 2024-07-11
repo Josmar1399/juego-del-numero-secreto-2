511ff00c74b2aaa129e2b475a88801ecb968a175
Josmar Guadarrama <al.221710905@gmail.com>
2024-07-10 22:51:40 -0600

    se generar repositorio e inicio del uso de selectores

---
+++ app.js
@@ -0,0 +1,6 @@
+let titulo = document.querySelector('h1'); 
+titulo.innerHTML = "Juego del número secreto";
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = "Dame un número del 1 al 10:";
+

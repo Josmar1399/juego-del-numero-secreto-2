9dbde56b740d6dbbe8fa8d2c619db2716f08fbea
Josmar Guadarrama <al.221710905@gmail.com>
2024-07-11 20:02:03 -0600

    se actualizó .js y .html para empezar a usar una function y onclick

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ titulo.innerHTML = "Juego del número secreto";
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = "Dame un número del 1 al 10:";
 
+function IntentoDeUsuario(){
+    alert('intento desde el botón');
+}

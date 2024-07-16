ef4712a8643316a4c56f0cdc15998ddf8687a372
Josmar Guadarrama <al.221710905@gmail.com>
2024-07-15 18:26:58 -0600

    se actualiza js para generar número secreto con una función

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let NumeroSecreto = generarNumeroSecreto();
+console.log(NumeroSecreto);
 
 function asignarTextoElemento (Elemento,Texto){
     let elementoHTML = document.querySelector(Elemento); 
@@ -8,5 +10,8 @@ function IntentoDeUsuario(){
     alert('intento desde el botón');
 }
 
+function generarNumeroSecreto (){
+    return Math.floor(Math.random()*10)+ 1; 
+}
 asignarTextoElemento ('h1', "Bienvenido al juego del número secreto");
 asignarTextoElemento ('p', "Dame un número del 1 al 100:");
